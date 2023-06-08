ae9d8355261ec931bc79c438c1f0e9465d877148
victd <55512729+victd@users.noreply.github.com>
2023-06-08 15:25:47 -0400

    Update trux.php

--- trux.php
+++ trux.php
@@ -10,7 +10,7 @@
 // put in facility to fix odometer corrections - AMCS sync tablet and Trux issue
 // internal ticket is dependent on the resolution of the external ticket
 // KM corrections and Fuel
-// Performance - MaintainIT and tire repair
+// Performance - MaintainIT and tire repair, patch, retread
 // provision for local serial or usb connections to cloud server or an on-prem local HQ
 // tomcat
 
